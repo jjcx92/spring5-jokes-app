9096b81ab871c100d0146f6f7f7f61a9c0ef5861
JJCX92 <jjcx92@gmail.com>
2019-08-31 10:58:11 -0600

    Java configuration example

--- src/main/java/guru/springframework/joke/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
-        public JokeServiceImpl() {
+        public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
             this.chuckNorrisQuotes= new ChuckNorrisQuotes();
         }
 
